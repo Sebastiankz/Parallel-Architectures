aa615e3139b52f3dbaabfbb00868011ce2b286b5
Sebastianolivo <sebastianolivo@uninorte.edu.co>
2025-05-26 13:27:45 -0500

    matmul.mpi.py termiando

--- MatrixM/matmul_mpi.py
+++ MatrixM/matmul_mpi.py
@@ -16,37 +16,43 @@ def main():
     N = int(sys.argv[1])
 
 
-    # Initialize matrices
-    rows_per_process = N // size # Number of rows each process will handle
-    A_part = np.empty((rows_per_process, N), dtype=np.float32) # Matrix A part
-    B = np.empty((N, N), dtype=np.float32) # Matrix B
-    C_part = np.empty((rows_per_process, N), dtype=np.float32) # Result matrix part
+    counts = [N // size + (1 if i < N % size else 0) for i in range(size)]
+    displs = [sum(counts[:i]) for i in range(size)]
 
-    # Broadcast the size of the matrix and initialize A and B
+    local_row = counts[rank] # Number of rows for this process
+
+    A_part = np.empty((local_row, N), dtype=np.float32)
+    B = np.random.rand(N, N).astype(np.float32)
+    C_part = np.empty((local_row, N), dtype=np.float32)
+
     if rank == 0:
         A = np.random.rand(N, N).astype(np.float32)
         B[:] = np.random.rand(N, N).astype(np.float32)
     else:
         A = None
 
-    # Scatter A and broadcast B
-    comm.Scatter([A, MPI.FLOAT], A_part, root=0)  # Scatter A to all processes
-    comm.Bcast([B, MPI.FLOAT], root = 0)  # Broadcast B to all processes
+    sendCounts = [r * N for r in counts] # Send counts in bytes
+    displs_bytes = [d * N for d in displs] # Displacements in bytes
+    comm.Scatterv([A, sendCounts, displs_bytes, MPI.FLOAT], A_part, root=0)
 
-    t0 = time.perf_counter()  # Start timer
-    C_part[:] = A_part @ B  # Perform local matrix multiplication
-    t1 = time.perf_counter()  # End timer
+    comm.Bcast([B, MPI.FLOAT], root=0)
+
+    t0 = time.perf_counter()
+    C_part[:] = A_part @ B
+    t1 = time.perf_counter()
 
     if rank == 0:
         C = np.empty((N, N), dtype=np.float32)
     else:
         C = None
 
-    comm.Gather(C_part, C, root=0) 
+    recvcounts = [r * N for r in counts]
+    recvdispls = [d * N for d in displs]
+    comm.Gatherv(C_part, [C, recvcounts, recvdispls, MPI.FLOAT], root=0)
 
     if rank == 0:
-       print(f"[MPI] {N}x{N} Tiempo: {t1 - t0:.6f} segundos", flush=True)
-
+        print(f"[MPI] {N}x{N} Tiempo: {t1 - t0:.6f} segundos", flush=True)
+    
 if __name__ == "__main__":
     main()
 
